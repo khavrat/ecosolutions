e558d7a834d865ae612c49411f009d2962051c18
khavrat <ihavrat@gmail.com>
2023-12-21 05:44:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,22 +20,6 @@ function App() {
         >
           Learn React
         </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>{" "}
         <a
           className="App-link"
           href="https://reactjs.org"
